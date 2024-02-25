1322d29e7daa3bff40c8484bde3e20dd1712b1d6
tracer-ysyx2204 <tracer@ysyx.org>
2024-02-25 17:50:38 +0800

    > generate verilog ysyx_22041213 张宇驰 Linux marinatoo-ASUS-TUF 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:50:38 up  2:22,  3 users,  load average: 0.98, 1.05, 1.28

--- .history/npc/playground/src/utils/Debug_20240207133558.scala
+++ .history/npc/playground/src/utils/Debug_20240207133558.scala
@@ -23,7 +23,7 @@ object LogUtil {
 //		val enableDisplay = WireInit(false.B)
 //		BoringUtils.addSink(enableDisplay, "DISPLAY_ENABLE")
 //		enableDisplay
-		false.B
+		true.B
 	}
 
 	def apply(debugLevel: LogLevel)
