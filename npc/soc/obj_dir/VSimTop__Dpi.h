41a82cd50d43b774ab9cdc97933dfb35777128e2
tracer-ysyx2204 <tracer@ysyx.org>
2024-03-11 22:15:46 +0800

    > sim RTL ysyx_22041213 张宇驰 Linux marinatoo-ASUS-TUF 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:15:46 up 22 min,  3 users,  load average: 0.48, 0.59, 0.55

---
+++ npc/soc/obj_dir/VSimTop__Dpi.h
@@ -0,0 +1,28 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Prototypes for DPI import and export functions.
+//
+// Verilator includes this file in all generated .cpp files that use DPI functions.
+// Manually include this file where DPI .c import functions are declared to ensure
+// the C functions match the expectations of the DPI imports.
+
+#ifndef VERILATED_VSIMTOP__DPI_H_
+#define VERILATED_VSIMTOP__DPI_H_  // guard
+
+#include "svdpi.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+    // DPI IMPORTS
+    // DPI import at /home/marinatoo/ysyx-workbench/npc/playground/resources/MEM.sv:15:51
+    extern unsigned long long vaddr_read(unsigned long long addr, int len);
+    // DPI import at /home/marinatoo/ysyx-workbench/npc/playground/resources/MEM.sv:20:39
+    extern void vaddr_write(unsigned long long addr, int len, unsigned long long data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // guard
