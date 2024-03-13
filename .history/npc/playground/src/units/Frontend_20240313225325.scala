aa89f6ed967086374a0966ee658101404d021be2
tracer-ysyx2204 <tracer@ysyx.org>
2024-03-13 22:55:02 +0800

    > generate verilog ysyx_22041213 张宇驰 Linux marinatoo-ASUS-TUF 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:55:02 up  2:24,  3 users,  load average: 1.65, 1.81, 1.81

--- .history/npc/playground/src/units/Frontend_20240313225325.scala
+++ .history/npc/playground/src/units/Frontend_20240313225325.scala
@@ -36,8 +36,7 @@ class Frontend_embedded(implicit val p: MarCoreConfig) extends MarCoreModule {
 	Debug("---------------------- Frontend ----------------------\n")
 	Debug("flush = %b, [ifu]<%x--=%x%x>[idu]<%x%x==\n",
 		ifu.io.flushVec.asUInt, ifu.io.out.valid,
-		idu.io.in(0).ready, idu.io.in(1).ready,
-		idu.io.out(0).valid, idu.io.out(1).valid)
+		idu.io.in(0).valid, idu.io.in(0).ready)
 	Debug(ifu.io.out.valid, 
 		"IFU: pc = 0x%x, instr = 0x%x\n",
 		ifu.io.out.bits.pc, ifu.io.out.bits.instr)
