04be9a4a32be40fdbfce030fcbd5b8f6f295409e
tracer-ysyx2204 <tracer@ysyx.org>
2024-03-19 20:52:59 +0800

    > compile NEMU ysyx_22041213 张宇驰 Linux marinatoo-ASUS-TUF 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:52:59 up 50 min,  2 users,  load average: 1.45, 1.40, 1.42

--- .history/nemu/src/memory/csim_20240319205126.c
+++ .history/nemu/src/memory/csim_20240319205126.c
@@ -164,8 +164,7 @@ paddr_t get_cache_free_line(uint8_t level, paddr_t addr, bool *isWriteBack) {
 
 	/* 从当前的Set中到找空闲的way(line)
 		cacheline_free_num统计整个Cache的可用块 */
-	for (size_t w = 0; w < cache->lv[level].way_num; w++) {
-		Log("cond %ld < %x", w, cache->lv[level].way_num);
+	for (size_t w = 0; w < cache->lv[level].way_num; w++) { 
 		if (!(cache->lv[level].line[index][w].valid)) {
 			if (cache->lv[level].cache_free_num > 0)
 				cache->lv[level].cache_free_num--;
