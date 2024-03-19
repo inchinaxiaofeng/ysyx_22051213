7837c7ddb75bf43dbe0b5955e7febd74327d28fe
tracer-ysyx2204 <tracer@ysyx.org>
2024-03-19 20:57:46 +0800

    > compile NEMU ysyx_22041213 张宇驰 Linux marinatoo-ASUS-TUF 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:57:46 up 55 min,  2 users,  load average: 1.56, 1.41, 1.40

--- .history/nemu/src/memory/csim_20240319205631.c
+++ .history/nemu/src/memory/csim_20240319205631.c
@@ -472,7 +472,6 @@ word_t do_cache_op(paddr_t addr, char oper_style, int byte_len, word_t write_dat
 			if (0 != last_get_line_byte_len) {
 				hit_way_l1 = check_cache_hit(0, addr+i*cls, &hit_l1);
 				if (!hit_l1) {
-					Log("AAA");
 					hit_way_l1 = get_cache_free_line(0, addr+i*cls, &hit_l1_wb);
 					do_cache_update_line(0, index+i, hit_way_l1, tag, hit_l1_wb);
 				}
