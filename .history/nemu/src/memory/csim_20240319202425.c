e6629a3131254978ebb1dfb25ad09eabfd08a432
tracer-ysyx2204 <tracer@ysyx.org>
2024-03-19 20:24:39 +0800

    > compile NEMU ysyx_22041213 张宇驰 Linux marinatoo-ASUS-TUF 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:24:39 up 22 min,  2 users,  load average: 1.59, 1.61, 1.31

--- .history/nemu/src/memory/csim_20240319202425.c
+++ .history/nemu/src/memory/csim_20240319202425.c
@@ -302,7 +302,7 @@ bool do_cache_write_line(
 	uint8_t level, paddr_t offset, paddr_t index,
 	paddr_t way, uint8_t* write_data, int byte_len
 ) {
-	Log("arg check: offset %x index %x way %x write_date %x", offset, index, way, *write_data);
+	Log("arg check: offset %x index %x way %x write_date", offset, index, way, *write_data);
 	uint32_t cls = cache->lv[level].cache_line_size;
 	bool isInsideLine = cls-offset >= byte_len;
 	for (size_t i = 0; i < isInsideLine?byte_len:cls-offset; i++)
